a77800b9b4a29daa9b3ae9d218e46bc51532d62a
iamshamit <shamitmishra22@gmail.com>
2026-06-08 17:36:38 +0530

    feat: pre-warm gallery-dl at startup, create download notification channel

--- android/app/src/main/java/com/instadown/app/InstaDownApp.kt
+++ android/app/src/main/java/com/instadown/app/InstaDownApp.kt
@@ -1,8 +1,12 @@
 package com.instadown.app
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import kotlin.concurrent.thread
 
 class InstaDownApp : Application() {
     override fun onCreate() {
@@ -11,6 +15,29 @@ class InstaDownApp : Application() {
         ThemePrefs.init(this)
         if (!Python.isStarted()) {
             Python.start(AndroidPlatform(this))
+        }
+        createNotificationChannel()
+        thread(isDaemon = true, name = "gdl-warmup") {
+            try {
+                Python.getInstance()
+                    .getModule("instadown_android")
+                    .callAttr("warmup")
+            } catch (_: Throwable) {}
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "instadown_downloads",
+                getString(R.string.notif_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT,
+            ).apply {
+                enableVibration(false)
+                setSound(null, null)
+            }
+            getSystemService(NotificationManager::class.java)
+                .createNotificationChannel(channel)
         }
     }
 
