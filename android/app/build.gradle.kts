f4acde02b30585698bb09a468eafa86f624ebba0
iamshamit <shamitmishra22@gmail.com>
2026-06-08 17:33:22 +0530

    build: add WorkManager dep, transparent theme, notification strings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -91,4 +91,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
 
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
